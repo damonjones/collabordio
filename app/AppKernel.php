a35b690281792ae7ee504219ba06597b2ff3932e
Damon Jones <damon@damonjones.me>
2013-02-08 18:29:40 -0500

    Add a bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new MDJ\LayoutBundle\MDJLayoutBundle(),
+            new HT\AppBundle\HTAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/HT/AppBundle/HTAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HT\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HTAppBundle extends Bundle
+{
+}
